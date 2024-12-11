26f90fa34072588da8eeb05743dacbecc2c2b4ea
Samuel Aliprandi <93950256+samuelaliprandi@users.noreply.github.com>
2024-12-11 15:00:31 +0100

    Delete JAX-RIEM-LA/solvers_comparison directory

--- JAX-RIEM-LA/solvers_comparison/run_banana_experiments.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-python banana_experiment.py --seed 0  --samples 100   --diffrax True --epochs 2000 --savefig True --solver dopri5 --pcoeff 0.2 --icoeff 0.3
-python banana_experiment.py --seed 0  --samples 100   --diffrax True --epochs 2000 --savefig True --solver tsit5 --pcoeff 0.2 --icoeff 0.3
-python banana_experiment.py --seed 0  --samples 100   --diffrax True --epochs 2000 --savefig True --solver bosh3 --pcoeff 0.2 --icoeff 0.3
